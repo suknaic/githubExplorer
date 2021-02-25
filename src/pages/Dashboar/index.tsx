bd79b6f70fa2e6cc9be64abb1003fe2aedb01b86
Felipe Suknaic <fsuknaic@hotmail.com>
2021-02-25 14:54:24 -0500

    pagina de dashboard

--- src/pages/Dashboar/index.tsx
+++ src/pages/Dashboar/index.tsx
@@ -1,7 +1,70 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logo} alt="github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do Repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/15646369?s=460&u=01d25984a387bef32d6066e7a449075b79dd8bd4&v=4"
+            alt="felipe suknaic"
+          />
+
+          <div>
+            <strong>Felipe Suknaic</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+              dolores.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/15646369?s=460&u=01d25984a387bef32d6066e7a449075b79dd8bd4&v=4"
+            alt="felipe suknaic"
+          />
+
+          <div>
+            <strong>Felipe Suknaic</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+              dolores.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/15646369?s=460&u=01d25984a387bef32d6066e7a449075b79dd8bd4&v=4"
+            alt="felipe suknaic"
+          />
+
+          <div>
+            <strong>Felipe Suknaic</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+              dolores.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
